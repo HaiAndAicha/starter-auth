6da09c42b3e96789b0384aa9a1eb1b82ecc64550
hahoanghai <hahoanghai.ueh@gmail.com>
2016-03-24 11:31:49 -0700

    step 4 authorization

--- application/config/constants.php
+++ application/config/constants.php
@@ -3,6 +3,9 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
+define('ROLE_USER','user');
+define('ROLE_ADMIN','admin');
+
 // location of data folders
 define('DATAPATH', './data/');
 define('XMLSUFFIX', '.xml');
